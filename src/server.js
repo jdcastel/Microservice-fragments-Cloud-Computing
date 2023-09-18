f43b154353393699a2562cb4971371f25c2ad8d8
maoruiz24 <maoruiz24@hotmail.com>
2023-09-18 00:06:44 -0400

    Switch to src/index.js as main entry point

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const logger = require('./logger');
 const app = require('./app');
 
 // Get the desired port from the process environment. Default to `8080`
-const port = parseInt(process.env.PORT || 8080, 10);
+const port = parseInt(process.env.PORT || 9000, 10);
 
 // Start a server listening on this port
 const server = stoppable(
