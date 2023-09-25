7e16e00a59cd7ffd744203b493538a367c39045f
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2023-09-25 10:51:50 -0400

    Workflow and unit test week 3

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@
 const express = require('express');
 
 // version and author from package.json
-const { version, author } = require('../../package.json');
+const { version, author } = require('../package.json');
+// const { authenticate } = require('passport');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+const { authenticate } = require('../src/auth');
+// const { createSuccessResponse } = require('../response');
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
-router.use(`/v1`, require('./api'));
+router.use(`/v1`, authenticate(), require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
