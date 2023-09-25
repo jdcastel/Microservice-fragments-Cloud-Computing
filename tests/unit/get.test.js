cc5bd2b0a4f1ad2145beb89942faaec67a6b8947
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2023-09-25 10:56:04 -0400

    Break and Fix your Unit Tests

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -5,8 +5,8 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('GET /v1/fragments', () => {
-  // If the request is missing the Authorization header, it should be forbidden
-  test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
+// If the request is missing the Authorization header, it should be forbidden
+test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(500));
 
   // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
