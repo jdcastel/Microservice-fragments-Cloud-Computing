e5cfa9cd40da99111d973e92bdcc3918aaa8e8cb
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2023-11-18 12:46:15 -0500

    add hostname to get

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 // version and author from package.json
 const { version, author } = require('../../package.json');
+const { hostname } = require('os');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
@@ -24,11 +25,14 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-res
-.status(200)
-.json(
-  createSuccessResponse({ author, githubUrl: 'https://github.com/jdcastel/fragments', version })
-);
+  res.status(200).json(
+    createSuccessResponse({ 
+      author, 
+      githubUrl: 'https://github.com/jdcastel/fragments', 
+      version,
+      hostname: hostname(), 
+    })
+  );
 }); 
 
 
