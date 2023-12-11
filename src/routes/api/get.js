7f4d42ace8a562cf82c663354783febd3402c00b
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2023-12-11 15:47:47 -0500

    fix expand

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,15 +1,17 @@
+// src/routes/api/get.js
 const { createSuccessResponse, createErrorResponse } = require('../../response');
-const { Fragment } = require('../../model/fragment');
+const { Fragment } = require("../../model/fragment");
 /**
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
   try {
-    let expand = req.query.expand && req.query.expand == 1 ? true : false;
-    var fragments = await Fragment.byUser(req.user, expand);
-
-    res.status(200).json(createSuccessResponse({ fragments }));
+    var fragments = await Fragment.byUser(req.user, req.query.expand == 1);
+    res.status(200).json(createSuccessResponse({
+      fragments
+    }));
   } catch (err) {
-    res.status(404).json(createErrorResponse(404, 'Error getting the fragment'));
+    res.status(404).json(createErrorResponse(404, err));
   }
+
 };
